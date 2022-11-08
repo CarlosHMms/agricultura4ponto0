a4af2c9f930bebb6fd08c529a99cd01ef657beea
CarlosHMms <107642033+CarlosHMms@users.noreply.github.com>
2022-11-08 15:15:00 -0300

    initial media query

--- sensordados.php
+++ sensordados.php
@@ -5,6 +5,9 @@
 	<title>Informações detalhadas</title>
 	<link rel="stylesheet" href="css/stylepage2.css">
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!--Configuração do media query -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- CSS Bootstrap -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> <!-- JavaScript Bundle with Popper -->
 </head>
